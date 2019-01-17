4c02d61dcfce3bc1368f7c0d0b2bb2d6d7238539
Kahaan <kahaan.r.patel@gmail.com>
2019-01-17 12:32:13 -0800

    adding components

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store.js";
+import App from "./components/app.jsx";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   window.store = store;
   const root = document.getElementById("content");
 
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  ReactDOM.render(<App />, root);
 });
