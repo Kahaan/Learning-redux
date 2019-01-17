e61f6e728e97158ef279b199ccac9262d44a3aae
Kahaan <kahaan.r.patel@gmail.com>
2019-01-17 10:42:56 -0800

    functional store

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,20 +1,34 @@
 import { RECEIVE_TODO, RECEIVE_TODOS } from "../actions/todo_actions";
+import { receiveTodo, receiveTodos } from "../actions/todo_actions";
 import merge from "lodash/merge";
-import RECEIVE_TODO from "../actions/todo_actions";
-import RECEIVE_TODOS from "../actions/todo_actions";
 
-const todosReducer = (state = {}, action) => {
-  Object.freeze(state);
-  let nextState = {};
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
+  let defaultState = {};
 
   switch (action.type) {
     case RECEIVE_TODOS:
       action.todos.forEach(todo => {
-        nextState[todo.id] = todo;
+        defaultState[todo.id] = todo;
       });
+      return defaultState;
     case RECEIVE_TODO:
-      const newTodo = { [action.todo.id]: action.todo };
-      return merge({}, state, newTodo);
+      state[action.todo.id] = action.todo;
+      return state;
     default:
       return state;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  window.store = configureStore();
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById("content");
 
   ReactDOM.render(<h1>Todos App</h1>, root);
